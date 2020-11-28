2cf4c325dd83a0cda74101d5fc42469f626bfb66
Mykel Kochenderfer <mykel@stanford.edu>
2020-11-28 11:40:09 -0800

    have keyword reserved match variable
    e causes problems

--- pygments_style_algfordm/algfordm.py
+++ pygments_style_algfordm/algfordm.py
@@ -26,6 +26,7 @@ class AlgForDMStyle(Style):
         Keyword:                   "bold #0072B2",
         Keyword.Pseudo:            "nobold",
         Keyword.Type:              "nobold #0072B2",
+        Keyword.Reserved           "#19177C",
         Keyword.Other:             "bold #73C6F2",
         Other:                     "bold #73C6F2",
 
